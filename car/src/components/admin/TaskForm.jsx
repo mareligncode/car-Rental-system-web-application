b01ec889d2bb96622f51d946e8d342cfc340bd52
mareligncode <yimermarelign@gmail.com>
2025-09-04 13:32:28 +0300

    Update TaskForm.jsx

--- car/src/components/admin/TaskForm.jsx
+++ car/src/components/admin/TaskForm.jsx
@@ -15,7 +15,7 @@ const TaskForm = ({ employees, onTaskAdded }) => {
         try {
             const token = localStorage.getItem('token');
             const taskData = { description, assignedTo };
-            await axios.post('http://localhost:5000/api/tasks', taskData, {
+            await axios.post('https://car-rental-system-web-application.onrender.com/api/tasks', taskData, {
                 headers: { Authorization: `Bearer ${token}` }
             });
             toast.success("Task assigned successfully!");
