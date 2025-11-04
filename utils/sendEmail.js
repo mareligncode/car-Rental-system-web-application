a87b190d552a935e2ee1e07d025e5f9b0f9d79d9
mareligncode <yimermarelign@gmail.com>
2025-11-04 12:05:00 +0300

    Configure TLS for email transport
    Added TLS configuration to avoid self-signed certificate issues.

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,10 +3,15 @@ const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
+            secure: false, // 👈 IMPORTANT for port 587
+
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
+          tls: {
+      rejectUnauthorized: false, // 👈 helps avoid self-signed cert issues
+    },
     });
     const mailOptions = {
         from: 'RentACar Support <support@rentacar.com>',
